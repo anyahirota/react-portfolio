d42c60ef104c0828650213ca22e9db614cb94cfb
anyahirota <anyahirota@gmail.com>
2023-05-26 16:47:26 -0700

    start on resume page and add contact in footer

--- anyas-website/src/pages/Resume.js
+++ anyas-website/src/pages/Resume.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
+import { Document, Page } from "react-pdf";
 
 const Resume = () => {
   return (
     <div>
-      Resume
+      <h1 className="page-title-margin">
+        <span className="page-title">Resume</span>
+      </h1>
+      <div className="resume-container">
+        <img
+          className="pic_of_resume"
+          alt="resume"
+          src="images/resume_placeholder.jpg"
+        ></img>
+        {/* <Document
+          file={resume}
+          // onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={1} />
+        </Document> */}
+      </div>
     </div>
   );
 };
